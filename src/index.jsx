78cca71f182834e09f6aa0125c795df791fedd2c
Andrii Lundiak <landike@gmail.com>
2019-04-08 02:58:30 +0200

    Changed code and rebuilt dist to run CheckPreview on GitHub page.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from 'components/App';
+import App from 'components/App';
 // import TestObj from 'components/Test';
 // import { Test } from 'components/Test';
 // import ReactColorSquare from 'react-color-square';
 // import ReactSum from '@lundiak/react-sum';
-import { Count, Sum } from '@lundiak/react-sum';
+// import { Count, Sum } from '@lundiak/react-sum';
 // import ReactSum from 'reactSum'; // Using Webpack Alias DIRECTLY to node_modules
 
 // TODO
@@ -14,19 +14,19 @@ import { Count, Sum } from '@lundiak/react-sum';
 // TODO
 import 'css/app.less';
 
-// ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 // ReactDOM.render(<Test />, document.getElementById('app'));
 
-ReactDOM.render(
-  <React.Fragment>
-    {/* <ReactColorSquare /> */}
-    {/* <ReactSum.Sum /> */}
-    {/* <ReactSum.Count /> */}
-    <Sum a={3} b={3} />
-    <Count list={[1, 2, 3]} />
-  </React.Fragment>,
-  document.getElementById('app')
-);
+// ReactDOM.render(
+//   <React.Fragment>
+//     {/* <ReactColorSquare /> */}
+//     {/* <ReactSum.Sum /> */}
+//     {/* <ReactSum.Count /> */}
+//     <Sum a={3} b={3} />
+//     <Count list={[1, 2, 3]} />
+//   </React.Fragment>,
+//   document.getElementById('app')
+// );
 
 if (module.hot) {
   module.hot.accept();
